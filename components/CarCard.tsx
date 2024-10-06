89318781560ba2c6cab0b8a400e0f39bd8c5372d
AnhNguyen <rosenguyen1027@gmail.com>
2024-10-06 16:42:39 +0300

    init CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { CarProps } from '@/types'
 import CustomButton from './CustomButton'
 import { calculationCarRent } from '@/utils'
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
     car: CarProps;
@@ -79,6 +80,8 @@ const CarCard = ({car}: CarCardProps) => {
             />
         </div>
       </div>
+
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
     </div>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,4 @@ export interface CarProps {
     transmission: string;
     year: number
 }
+
